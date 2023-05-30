7cc55dfb386cbfbb27c5e61b2a629ce2e62ae716
daniellberg <danielbergons@gmail.com>
2023-05-29 21:23:57 -0300

    tried change ahref color on click

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const colors = ["green", "red", "grey", "maroon", "blue", "yellow", "purple", "p
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+const nav = document.querySelector(".nav-btn");
 
 btn.addEventListener('click', function (){
 
@@ -11,6 +12,11 @@ btn.addEventListener('click', function (){
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
+
+nav.addEventListener('click', function(){
+    document.getElementById("nav-btn-id").style.color = "#ff0000"
+});
+
 
 function getRandomNumber(){
     return Math.floor(Math.random()*colors.length);
